2c49ee084da2b958227d7e152c5657751f833f14
Atiya Afzal <atiyajhg@gmail.com>
2026-05-14 12:30:05 +0500

    authorization added

---
+++ labTask-3/public/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    if (navLinks.style.display === "flex") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "flex";
+    }
+});
